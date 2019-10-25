28c056e9762d0a09997ddf4d330f3144c9e6a6e8
王贝 <wangbei@wayyue.com>
2019-10-25 11:42:31 +0800

    typescript

---
+++ vue-ts/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <Demo />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+// import HelloWorld from './components/HelloWorld.vue'
+import Demo from './components/Demo.vue';
+
+@Component({
+  components: {
+    Demo
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-ts/src/components/Demo.vue
@@ -0,0 +1,94 @@
+/* eslint-disable semi */
+/* ------------------------------------------- template ------------------------------------------- */
+<template>
+  <div>
+    <div>{{sWord}}</div>
+    <div>1. 什么是 Typescript?</div>
+    <div>2. Typescript 的优缺点</div>
+    <div>3. Typescript 基础</div>
+  </div>
+</template>
+
+/* -------------------------------------------- script -------------------------------------------- */
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+
+@Component
+export default class Demo extends Vue {
+  sWord: string = 'Hello Typescript!'; // sWord 内部属性
+
+  /* ----------------------1. 生命周期函数------------------------ */
+  created () {
+    this.showMeCode();
+  }
+
+  /* ----------------------2. 监听属性函数------------------------ */
+  // 监听 sWord 是否发生改变
+  @Watch('sWord')
+  onChangeValue (newV: string, oldV: string) {
+    if (newV !== oldV) {
+      alert('监控状态已经发生改变！！！');
+    } else {
+      alert('监控状态尚未发生改变！！！');
+    }
+  }
+
+  mounted (): void {
+    setTimeout(() => {
+      this.sWord = 'Typescript Demo页面';
+    }, 1500);
+  }
+
+  /* ----------------------3. 事件调用函数------------------------ */
+
+  /* ----------------------4. 内部功能函数------------------------ */
+  showMeCode () {
+    // 原始数据类型
+    this.showDataTypeCode();
+
+    // 任意值
+    this.showAnyTypeCode();
+  }
+
+  // 原始数据类型
+  showDataTypeCode () {
+    // boolean
+    let isDone: boolean = false;
+    // let createdByNewBoolean1: Boolean = new Boolean(1);
+    // console.log('createdByNewBoolean1 =' + createdByNewBoolean1);
+    let createdByNewBoolean2: Boolean = Boolean(1);
+    console.log('createdByNewBoolean2 =' + createdByNewBoolean2);
+    let createdByNewBoolean3: boolean = Boolean(1);
+    console.log('createdByNewBoolean3 =' + createdByNewBoolean3);
+
+    // 数值
+    let nDecLiteral: number = 7;
+    let nhexLiteral: number = 0xf00d;
+    let nbinaryLiteral: number = 0b1010;
+    let nOctalLiteral: number = 0o744;
+    let nNotANumber: number = NaN;
+    let nInfinityNumber: number = Infinity;
+
+    // 字符串
+    let sName: string = 'begodya';
+    let nAge: number = 17;
+    let sSentence: string = `Hello, my name is ${sName}. I'll be ${nAge +
+      1} years old next month.`;
+    console.log(sSentence);
+
+    // 空值、Null、Undefined
+    let unusale: void;
+    let u: undefined;
+    let n: null = null;
+  }
+
+  // 任意值
+  showAnyTypeCode () {}
+
+  /* ----------------------5. 服务请求函数------------------------ */
+}
+</script>
+
+/* -------------------------------------------- style -------------------------------------------- */
+<style scoped lang="scss">
+</style>
