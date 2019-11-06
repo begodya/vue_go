574f043cb50385870abe81c708bcc6910ef70d2b
王贝 <wangbei@wayyue.com>
2019-11-06 16:24:57 +0800

    typescript

--- vue-ts/src/components/Demo.vue
+++ vue-ts/src/components/Demo.vue
@@ -12,38 +12,39 @@
 
 /* -------------------------------------------- script -------------------------------------------- */
 <script lang="ts">
-import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
 
 @Component
 export default class Demo extends Vue {
-  private sWord: string = 'Hello Typescript!'; // sWord 内部属性
+  private sWord: string = "Hello Typescript!"; // sWord 内部属性
 
   /* ----------------------1. 生命周期函数------------------------ */
-  created () {
+  created() {
     this.showMeCode();
+    s;
   }
 
   /* ----------------------2. 监听属性函数------------------------ */
   // 监听 sWord 是否发生改变
-  @Watch('sWord')
-  onChangeValue (newV: string, oldV: string) {
+  @Watch("sWord")
+  onChangeValue(newV: string, oldV: string) {
     if (newV !== oldV) {
-      alert('监控状态已经发生改变！！！');
+      alert("监控状态已经发生改变！！！");
     } else {
-      alert('监控状态尚未发生改变！！！');
+      alert("监控状态尚未发生改变！！！");
     }
   }
 
-  mounted (): void {
+  mounted(): void {
     setTimeout(() => {
-      this.sWord = 'Typescript Demo页面';
+      this.sWord = "Typescript Demo页面";
     }, 1500);
   }
 
   /* ----------------------3. 事件调用函数------------------------ */
 
   /* ----------------------4. 内部功能函数------------------------ */
-  showMeCode (): void {
+  showMeCode(): void {
     // 原始数据类型
     this.showDataTypeCode();
 
@@ -61,11 +62,11 @@ export default class Demo extends Vue {
 
     // 函数类型
     let name = this.showFuncTypeCode();
-    console.log('name = ' + name);
+    console.log("name = " + name);
 
     // 类型断言
     let type = this.showTypeAssertionCode(7);
-    console.log('type = ' + type);
+    console.log("type = " + type);
 
     // 类型别名
     this.showTypeAliasCode();
@@ -80,20 +81,20 @@ export default class Demo extends Vue {
     this.showClassInterfaceCode();
 
     // 泛型
-    let genericsCode = this.showGenericsCode([7, 'seven']);
-    console.log('genericsCode = ' + genericsCode);
+    let genericsCode = this.showGenericsCode([7, "seven"]);
+    console.log("genericsCode = " + genericsCode);
   }
 
   // 原始数据类型
-  showDataTypeCode (): void {
+  showDataTypeCode(): void {
     // boolean
     let isDone: boolean = false;
     // let createdByNewBoolean1: Boolean = new Boolean(1);
     // console.log('createdByNewBoolean1 =' + createdByNewBoolean1);
     let createdByNewBoolean2: Boolean = Boolean(1);
-    console.log('createdByNewBoolean2 =' + createdByNewBoolean2);
+    console.log("createdByNewBoolean2 =" + createdByNewBoolean2);
     let createdByNewBoolean3: boolean = Boolean(1);
-    console.log('createdByNewBoolean3 =' + createdByNewBoolean3);
+    console.log("createdByNewBoolean3 =" + createdByNewBoolean3);
 
     // 数值
     let nDecLiteral: number = 7;
@@ -104,7 +105,7 @@ export default class Demo extends Vue {
     let nInfinityNumber: number = Infinity;
 
     // 字符串
-    let sName: string = 'begodya';
+    let sName: string = "begodya";
     let nAge: number = 17;
     let sSentence: string = `Hello, my name is ${sName}. I'll be ${nAge +
       1} years old next month.`;
@@ -117,33 +118,33 @@ export default class Demo extends Vue {
   }
 
   // 任意值
-  showAnyTypeCode (): void {
-    let myFavoriteNumber: any = 'seven';
+  showAnyTypeCode(): void {
+    let myFavoriteNumber: any = "seven";
     console.log(`myFavoriteNumber = ${myFavoriteNumber}`);
     myFavoriteNumber = 7;
     console.log(`myFavoriteNumber = ${myFavoriteNumber}`);
   }
 
   // 类型推论
-  showTypeInferenceCode (): void {
+  showTypeInferenceCode(): void {
     // let myFavoriteNumber = 'SEVEN';
     // myFavoriteNumber = 7;
 
     let myFavoriteNumber;
-    myFavoriteNumber = 'seven';
+    myFavoriteNumber = "seven";
     myFavoriteNumber = 7;
   }
 
   // 联合类型
-  showUnionTypesCode (): void {
+  showUnionTypesCode(): void {
     let myFavoriteNumber: string | number;
-    myFavoriteNumber = 'seven';
+    myFavoriteNumber = "seven";
     myFavoriteNumber = 7;
     // myFavoriteNumber = true;
   }
 
   // 对象类型接口
-  showInterfaceCode (): void {
+  showInterfaceCode(): void {
     interface Person {
       name: string;
       age: number;
@@ -151,36 +152,36 @@ export default class Demo extends Vue {
     }
 
     let tom: Person = {
-      name: 'Tom',
+      name: "Tom",
       age: 18
     };
   }
 
   // 数组类型
-  showArrayCode (): void {
+  showArrayCode(): void {
     let aFoo: number[] = [1, 2, 3, 4, 5, 6];
     // let aFoo: number[] = [1, 2, 3, 4, 5, '6'];
-    console.log('aFoo = ' + aFoo);
+    console.log("aFoo = " + aFoo);
 
     let aGoo: Array<number> = [7, 8, 9, 10, 11];
-    console.log('aGoo = ' + aGoo);
+    console.log("aGoo = " + aGoo);
 
-    let aHoo: Array<any> = ['A', 0, true, { url: 'www.shanzhen.me' }]; // any[]
-    console.log('aHoo = ' + aHoo);
+    let aHoo: Array<any> = ["A", 0, true, { url: "www.shanzhen.me" }]; // any[]
+    console.log("aHoo = " + aHoo);
   }
 
   // 函数类型：默认值、可选型
-  showFuncTypeCode (firstName: string = 'cat', lastName?: string): string {
+  showFuncTypeCode(firstName: string = "cat", lastName?: string): string {
     if (lastName) {
-      return firstName + ' ' + lastName;
+      return firstName + " " + lastName;
     } else {
       return firstName;
     }
   }
 
   // 类型断言：不是类型转换，而是手动指定一个类型
-  showTypeAssertionCode (something: string | number): number {
-    console.log('something = ' + something);
+  showTypeAssertionCode(something: string | number): number {
+    console.log("something = " + something);
     if ((something as string).length) {
       return (something as string).length;
     } else {
@@ -191,29 +192,29 @@ export default class Demo extends Vue {
   /* Typescript 进阶 */
 
   // 类型别名：常用于联合类型
-  showTypeAliasCode (): void {
+  showTypeAliasCode(): void {
     type TypeAlias = string | number;
-    let name: TypeAlias = 'begodya';
-    console.log('name = ' + name);
+    let name: TypeAlias = "begodya";
+    console.log("name = " + name);
     let age: TypeAlias = 18;
-    console.log('age = ' + age);
+    console.log("age = " + age);
   }
 
   // 元组：合并不同类型的对象、any[]
-  showTupleCode (): void {
-    let tom: [string, number] = ['Tom', 18];
+  showTupleCode(): void {
+    let tom: [string, number] = ["Tom", 18];
     // tom.push('male');
     // tom.push(true);
-    console.log('tom = ' + tom[0] + tom[1]);
+    console.log("tom = " + tom[0] + tom[1]);
 
-    let jerry: any[] = ['Jerry', 10];
-    jerry.push('male');
+    let jerry: any[] = ["Jerry", 10];
+    jerry.push("male");
     jerry.push(true);
-    console.log('jerry = ' + jerry[0] + jerry[1]);
+    console.log("jerry = " + jerry[0] + jerry[1]);
   }
 
   // 枚举类型：用于取值限定在一定范围内的场景
-  showEnumCode (): void {
+  showEnumCode(): void {
     enum Days {
       Sun = 7,
       Mon = 1,
@@ -224,11 +225,11 @@ export default class Demo extends Vue {
       Sat
     }
 
-    console.log('Days =', Days.Sun);
+    console.log("Days =", Days.Sun);
   }
 
   // 类
-  showClassCode (): void {
+  showClassCode(): void {
     // 类：定义了一个事物的抽象特点，包括属性和方法
     // 对象：类的实例，通过 new 生成
     // 面向对象三大特性：封装、继承、多态
@@ -239,39 +240,39 @@ export default class Demo extends Vue {
 
     class Animal {
       public name: string;
-      public constructor (name: string) {
+      public constructor(name: string) {
         this.name = name;
       }
 
-      public sayHi (): string {
+      public sayHi(): string {
         return `My name is ${name}`;
       }
 
       // abstract public sayHi();
     }
 
-    let animal = new Animal('Mouse');
+    let animal = new Animal("Mouse");
     console.log(animal.name);
-    animal.name = 'Jerry';
+    animal.name = "Jerry";
     console.log(animal.name);
 
     class Cat extends Animal {
-      constructor (name: string) {
+      constructor(name: string) {
         super(name);
         console.log(`Hello ${name}`);
       }
 
-      public sayHi (): string {
+      public sayHi(): string {
         return `Nice to meet you, I'm ${name}`;
       }
     }
 
-    let cat = new Cat('Tom');
+    let cat = new Cat("Tom");
     console.log(cat.sayHi());
   }
 
   // 类与接口
-  showClassInterfaceCode (): void {
+  showClassInterfaceCode(): void {
     // 类实现接口
     // 接口继承接口
     // 接口继承类
@@ -283,20 +284,20 @@ export default class Demo extends Vue {
     class Animal {}
 
     class Dog extends Animal implements Voice {
-      say () {
-        console.log('汪汪汪~~~~');
+      say() {
+        console.log("汪汪汪~~~~");
       }
     }
 
     class Person implements Voice {
-      say () {
-        console.log('Come here~');
+      say() {
+        console.log("Come here~");
       }
     }
   }
 
   // 泛型：输入与输出保持类型一致：Array<any>、Array<T>
-  showGenericsCode<T, U> (tuple: [T, U]): [U, T] {
+  showGenericsCode<T, U>(tuple: [T, U]): [U, T] {
     return [tuple[1], tuple[0]];
   }
 
